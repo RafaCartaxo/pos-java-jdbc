4834d2df40833bcde1b628d512d8758b2d8b2dfc
Rafael Cartaxo <Rafael.cartaxo@hotmail.com>
2023-02-09 18:47:30 -0300

    JDBC git início

---
+++ pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/App.java
@@ -0,0 +1,15 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Olá mundo!" );
+        System.out.println( "Hello world!" );
+        
+    }
+}
